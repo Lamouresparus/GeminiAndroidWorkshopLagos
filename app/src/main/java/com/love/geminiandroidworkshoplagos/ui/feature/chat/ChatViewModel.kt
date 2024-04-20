191b72cf7ad9227a30e93eafdc3cd6bb9a023b3a
loveotudor <loveotudor@gmail.com>
2024-04-20 10:58:46 +0100

    Add ChatViewModel.kt

---
+++ app/src/main/java/com/love/geminiandroidworkshoplagos/ui/feature/chat/ChatViewModel.kt
@@ -0,0 +1,29 @@
+package com.love.geminiandroidworkshoplagos.ui.feature.chat
+
+import androidx.compose.runtime.toMutableStateList
+import androidx.lifecycle.ViewModel
+
+class ChatViewModel() : ViewModel() {
+
+
+}
+
+class ChatUiState(
+    messages: List<ChatMessage> = emptyList()
+) {
+    private val _messages: MutableList<ChatMessage> = messages.toMutableStateList()
+    val messages: List<ChatMessage> = _messages
+
+    fun addMessage(msg: ChatMessage) {
+        _messages.add(msg)
+    }
+
+    fun replaceLastPendingMessage() {
+        val lastMessage = _messages.lastOrNull()
+        lastMessage?.let {
+            val newMessage = lastMessage.apply { isPending = false }
+            _messages.removeLast()
+            _messages.add(newMessage)
+        }
+    }
+}
