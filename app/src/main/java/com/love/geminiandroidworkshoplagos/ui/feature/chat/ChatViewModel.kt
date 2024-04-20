f4cad1f6a7b9d0b29803962048c69b740fbbbe30
loveotudor <loveotudor@gmail.com>
2024-04-20 13:41:39 +0100

    Add viewsState for Chat

--- app/src/main/java/com/love/geminiandroidworkshoplagos/ui/feature/chat/ChatViewModel.kt
+++ app/src/main/java/com/love/geminiandroidworkshoplagos/ui/feature/chat/ChatViewModel.kt
@@ -2,9 +2,17 @@ package com.love.geminiandroidworkshoplagos.ui.feature.chat
 
 import androidx.compose.runtime.toMutableStateList
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
-class ChatViewModel() : ViewModel() {
+class ChatViewModel : ViewModel() {
 
+    private val _uiState: MutableStateFlow<ChatUiState> =
+        MutableStateFlow(ChatUiState())
+
+    val uiState: StateFlow<ChatUiState> =
+        _uiState.asStateFlow()
 
 }
 
